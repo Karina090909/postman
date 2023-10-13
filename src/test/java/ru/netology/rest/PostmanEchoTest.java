ce86984e90de627b97251eb2578a47a57f3adb0d
Karina <not-funny@mail.ru>
2023-10-13 17:54:42 +0700

    initial commit

---
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -0,0 +1,21 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class PostmanEchoTest {
+
+    @Test
+    void shouldReturnSendData(){
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("09.09.1999")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data",equalTo("09.09.1991"));
+    }
+
+}
